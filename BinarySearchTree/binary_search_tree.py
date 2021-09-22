0d18a03b87743a3c4e11f3d4b0f65f6ddd49ebf6
Hoang Phuc Luan Truong <83048295+truongluan303@users.noreply.github.com>
2021-09-22 01:59:45 -0700

    add binary tree delete

--- BinarySearchTree/binary_search_tree.py
+++ BinarySearchTree/binary_search_tree.py
@@ -1,3 +1,4 @@
+from collections import deque
 from typing import Generic, TypeVar
 
 T = TypeVar('T')
@@ -7,139 +8,335 @@ class BinarySearchTree(Generic[T]):
 
     def __init__(self) -> None:
         self.size = 0
-        self.height = None
+        self.height = -1
         self.root = None
+        
+
         
     
 
-    def insert(self, value):
+    def insert(self, value: T) -> bool:
+        '''
+        insert a new value to the binary search tree
+        @return: whether the function call changed the tree
+        '''
         new_node = self.Node(value)
+
         # if the tree is empty, then the new node will be the root
         if self.is_empty():
             self.root = new_node
             self.height = 0
+
         # else, look for the appropriate location for the new node
         else:
             # if tree is not empty, then we know there's at least 1 node which is root
             # and height is at least 0, which is root's level
             current = self.root
             height = 0
+
             # perform the BST insert logic
             while True:
+
                 height += 1
                 parent = current
+
                 if value > current.data:
                     current = current.right_child
+
                     if current is None:
                         parent.right_child = new_node
                         break
+
                 elif value < current.data:
+
                     current = current.left_child
+
                     if current is None:
                         parent.left_child = new_node
                         break
+
                 # BST does not allow duplicates
                 else:
-                    raise Exception("Duplicate found: " + str(value) + " !!!")
+                    return False
+
             if height >= self.height:
-                self.height = height + 1
+                self.height = height
+
         self.size += 1
 
+        return True
 
 
 
-    def search(self, value):
+
+
+    def search(self, value: T) -> 'Node':
+        '''
+        search for a node with specific value in the tree
+        @return: the node with the given value
+        '''
         if self.is_empty():
-            raise Exception("Attempted to search an empty tree!!!")
+            return None
+
         current = self.root
+
         while current is not None:
+
             if value > current.data:
                 current = current.right_child
+
             elif value < current.data:
                 current = current.left_child
+
             else:
                 return current
+
         return None
 
 
 
-    def breadth_first_traversal(self):
-        if self.is_empty():
-            raise Exception("Attempted to traverse an empty tree!!!")
-        for i in range (self.height):
-            self.__get_current_level(self.root, i)
+
+
+    def delete(self, value: T) -> bool:
+        '''
+        delete the node with the desired value
+        @return: whether the call changed the tree
+        '''
+        prev_size = self.size
+        self.__delete(self.root, value)
+
+        self.height = self.__calculate_tree_height(self.root)
+
+        if prev_size > self.size:
+            return True
         
+        return False
 
 
-    def preorder_traversal(self):
-        self.__preorder_traversal(self.root)
+    def __delete(self, root: 'Node', value: T):
+        '''
+        helper function for delete
+        '''
+        if root is None:
+            return None
+
+        if value > root.data:
+            root.right_child = self.__delete(root.right_child, value)
+
+        elif value < root.data:
+            root.left_child = self.__delete(root.left_child, value)
+
+        else:
+
+            if root.left_child is None:
+                temp = root.right_child
+                self.size -= 1
+                return temp
+
+            elif root.right_child is None:
+                temp = root.left_child
+                self.size -= 1
+                return temp
+
+            inorder_successor = self.__find_min_value_node(root.right_child)
+
+            root.data = inorder_successor.data
+
+            root.right_child = self.__delete(root.right_child, inorder_successor.data)
+
+        return root
 
 
-    def postorder_traversal(self):
-        self.__postorder_travelsal(self.root)
 
 
-    def inorder_traversal(self):
-        self.__inorder_traversal(self.root)
+
+    def __find_min_value_node(self, root: 'Node') -> 'Node':
+        '''
+        find the node with the minimum value in the subtree
+        @return: the node with minimum value
+        '''
+        current = root
+
+        while current.left_child is not None:
+            current = current.left_child
+
+        return current
 
 
-    def delete(self, value):
-        pass
 
 
-    def get_size(self):
+
+    def get_size(self) -> int:
+        '''
+        return the size of the tree
+        '''
         return self.size
 
 
-    def get_height(self):
+
+
+
+    def get_height(self) -> int:
+        '''
+        return the height of the tree
+        '''
         return self.height
 
     
-    def is_empty(self):
+
+
+
+    def is_empty(self) -> bool:
+        '''
+        return whether the tree is empty
+        '''
         return self.size == 0
 
     
-    def clear(self):
+
+
+
+    def clear(self) -> None:
+        '''
+        delete every nodes in the tree
+        '''
         self.root = None
         self.size = 0
 
 
-    def __get_current_level(self, parent, level):
+
+
+
+    def level_order_traversal(self) -> list[list[T]]:
+        '''
+        traverse the tree level by level and return the list of
+        values of the nodes in the tree level by level
+        '''
+        queue = deque()
+        queue.append(self.root)
+
+        arr = list()
+        temp = list()
+
+        while queue:
+            
+            if len(arr) == self.height + 1:
+                return arr
+
+            size = len(queue)
+
+            for _ in range(size):
+
+                current = queue.popleft()
+
+                if current is not None:
+                    temp.append(current.data)
+                    queue.append(current.left_child)
+                    queue.append(current.right_child)
+
+                else:
+                    temp.append(None)
+
+            arr.append(temp)
+            temp = list()
+
+        return arr
+
+
+
+
+
+    def inorder_traversal(self) -> list[T]:
+        '''
+        Perform an inorder traversal and return the list
+        of values of the nodes in the tree in inorder rule
+        '''
+        arr = list()
+        self.__inorder_traversal(self.root, arr)
+        return arr
+
+
+    def __inorder_traversal(self, parent: 'Node', arr: list[T]) -> None:
+        '''
+        helper function for inorder traversal
+        '''
         if parent is None:
             return
-        if level == 0:
-            print(parent.data, end='  ')
-        else:
-            self.__get_current_level(parent.left_child, level - 1)
-            self.__get_current_level(parent.right_child, level - 1)
-        
-    
-    # inorder: left -> parent -> right
-    def __inorder_traversal(self, parent):
-        if parent is None:
-            return
-        # use recursion to print the left child first
-        self.__inorder_traversal(parent.left_child)
-        print(parent.data, end='  ')
-        self.__inorder_traversal(parent.right_child)
+
+        # inorder traverse: left -> parent -> right
+
+        self.__inorder_traversal(parent.left_child, arr)
+        arr.append(parent.data)
+        self.__inorder_traversal(parent.right_child, arr)
 
     
-    # preorder: parent -> left -> right
-    def __preorder_traversal(self, parent):
-        if parent is None:
-            return
-        print(parent.data, end='  ')
-        self.__preorder_traversal(parent.left_child)
-        self.__preorder_traversal(parent.right_child)
 
 
-    # postorder: left -> right -> root
-    def __postorder_travelsal(self, parent):
+
+    def preorder_traversal(self) -> list[T]:
+        '''
+        Perform a preorder traversal and return the list of
+        values of the nodes in the tree in preorder rule
+        '''
+        arr = list()
+        self.__preorder_traversal(self.root, arr)
+        return arr
+
+
+    def __preorder_traversal(self, parent: 'Node', arr: list[T]) -> None:
+        '''
+        helper function for preorder traversal
+        '''
         if parent is None:
             return
-        self.__postorder_travelsal(parent.left_child)
-        self.__postorder_travelsal(parent.right_child)
-        print(parent.data, end='  ')
+
+        # preorder traverse: parent -> left -> right
+        arr.append(parent.data)
+        self.__preorder_traversal(parent.left_child, arr)
+        self.__preorder_traversal(parent.right_child, arr)
+
+
+
+
+
+    def postorder_traversal(self) -> list[T]:
+        '''
+        Perform a postorder traversal and return the list of
+        values of the nodes in the trees in postorder rule
+        '''
+        arr = list()
+        self.__postorder_travelsal(self.root, arr)
+        return arr
+
+    def __postorder_travelsal(self, parent: 'Node', arr: list[T]) -> None:
+        '''
+        helper function for post order traversal
+        '''
+        if parent is None:
+            return
+
+        # postorder traverse: left -> right -> root
+
+        self.__postorder_travelsal(parent.left_child, arr)
+        self.__postorder_travelsal(parent.right_child, arr)
+        arr.append(parent.data)
+
+
+
+
+
+    def __calculate_tree_height(self, root: 'Node') -> int:
+        '''
+        re-calculate the height for the tree
+        '''
+        if root is None:
+            return -1
+
+        left_height = self.__calculate_tree_height(root.left_child)
+        right_height = self.__calculate_tree_height(root.right_child)
+
+        return max(left_height, right_height) + 1
+
 
 
 
@@ -147,6 +344,7 @@ class BinarySearchTree(Generic[T]):
     #####################################
     ############ NODE class #############
     class Node(Generic[T]):
+
         def __init__(self, data, left=None, right=None) -> None:
             self.data = data
             self.left_child = left
--- BinarySearchTree/test_code.py
+++ BinarySearchTree/test_code.py
@@ -1,44 +1,83 @@
 from binary_search_tree import BinarySearchTree
 
 
+
+
+def print_tree_information(bst: 'BinarySearchTree'):
+
+    print()
+
+    print('Tree height:', bst.get_height())
+    print('Tree size:', bst.get_size())
+
+    print('\nLevel Order Traversal:')
+    print(bst.level_order_traversal())
+
+    print('\nInorder Traversal:')
+    print(bst.inorder_traversal())
+
+    print('\nPreorder Traversal:')
+    print(bst.preorder_traversal())
+
+    print('\nPostorder Traversal:')
+    print(bst.postorder_traversal())
+    
+
+
+
+
 def main():
+
     bst = BinarySearchTree[int]()
+
     print('\n\nFIRST BST:')
+    print('----------------------------------------------------')
+
     arr1 = [5, 1, 0, 4, 3, 6, 8, 7, 9]
+
     for i in arr1:
         bst.insert(i)
-    print('\nBreadth First Search:')
-    bst.breadth_first_traversal()
-    print('\nInorder Search:')
-    bst.inorder_traversal()
-    print('\nPreorder Search:')
-    bst.preorder_traversal()
-    print('\nPostorder Search:')
-    bst.postorder_traversal()
+
+    print(bst.height)
+
+    print_tree_information(bst)
     
+    
+    print('\n\n***** After removing 4, 7, 8, and 5...\n')
+
+    bst.delete(4)
+    bst.delete(7)
+    bst.delete(8)
+    bst.delete(5)
+
+    print_tree_information(bst)
+    
+
+
+
     bst.clear()
 
     print('\n\nSECOND BST:')
-    arr2 = [5, 6, 3, 4, 2]
+    print('----------------------------------------------------')
+
+    arr2 = [7, 6, 8, 4, 10, 20, 1, 16, 12, 11, 15, 3, 2]
+
     for i in arr2:
         bst.insert(i)
-    print('\nBreadth First Search:')
-    bst.breadth_first_traversal()
-    print('\nInorder Search:')
-    bst.inorder_traversal()
-    print('\nPreorder Search:')
-    bst.preorder_traversal()
-    print('\nPostorder Search:')
-    bst.postorder_traversal()
+
+    print_tree_information(bst)
+
+
+    print('\n\n***** After removing 7, and 8...\n')
+    
+    bst.delete(7)
+    bst.delete(8)
+
+    print_tree_information(bst)
+
+
+
 
 
 if __name__ == "__main__":
     main()
-
-
-###### The initial BST should look like:
-
-#                          5
-#                     1        6
-#                  0     4        8
-#                       3        7  9
